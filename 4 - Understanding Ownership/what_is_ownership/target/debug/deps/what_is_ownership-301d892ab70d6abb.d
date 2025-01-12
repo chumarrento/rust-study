3546f02f89850baff3777639c3710afe3fc30282
Lucas Pereira <lucasp28.contato@gmail.com>
2025-01-12 20:55:56 -0300

    wip

--- 4 - Understanding Ownership/what_is_ownership/target/debug/deps/what_is_ownership-301d892ab70d6abb.d
+++
@@ -1,5 +0,0 @@
-/home/lucas/ws/rust-book/4 - Understanding Ownership/what_is_ownership/target/debug/deps/libwhat_is_ownership-301d892ab70d6abb.rmeta: src/main.rs
-
-/home/lucas/ws/rust-book/4 - Understanding Ownership/what_is_ownership/target/debug/deps/what_is_ownership-301d892ab70d6abb.d: src/main.rs
-
-src/main.rs:
--- 4 - Understanding Ownership/what_is_ownership/target/debug/what_is_ownership.d
+++
@@ -1,1 +0,0 @@
-/home/lucas/ws/rust-book/4\ -\ Understanding\ Ownership/what_is_ownership/target/debug/what_is_ownership: /home/lucas/ws/rust-book/4\ -\ Understanding\ Ownership/what_is_ownership/src/main.rs
